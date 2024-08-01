bcb80c965983dba5e3a5d51c9816f9d78eafb46b
Right 97 <Right.97@rs.net>
2024-08-01 16:10:05 +0500

    vercel commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { ColorPicker, useColor } from "react-color-palette";
+import "react-color-palette/css";
+import downloadjs from 'downloadjs';
+import html2canvas from 'html2canvas';
 
 function App() {
   const [textOptions, setTextOptions] = useState({
@@ -10,10 +14,24 @@ function App() {
     width: 200,
     whitespaceBreak: false,
   });
+  const [color, setColor] = useColor("#000000");
+  const [fgcolor, setfgColor] = useColor("#ffffff");
+  const [ChoosebgColor, SetChoosebgColor] = useState(false);
+  const [ChoosefgColor, SetChoosefgColor] = useState(false);
+  
+  const handlebgColorPick = function(){
+    SetChoosebgColor(true);
+    SetChoosefgColor(false);
+  }
 
+  const handlefgColorPick = function(){
+    SetChoosefgColor(true);
+    SetChoosebgColor(false);
+  }
   const Layouts = ["default", "fitted", "full", "controlled smushing", "universal smushing"];
   const [newText, setNewText] = useState("");
   const [fonts, setFonts] = useState([]);
+  const DivRef = useRef();
 
   const handleSpaceChange = (event) => {
     const { name, value } = event.target;
@@ -45,7 +63,8 @@ function App() {
   const GetFonts = async () => {
     try {
       const res = await axios({
-        url: "https://ascii-banner-backend-murex.vercel.app",
+        // url: "https://ascii-banner-backend-murex.vercel.app",
+        url: "http://localhost:8000",
         method: "get",
       });
       if (res.data.data) {
@@ -60,17 +79,24 @@ function App() {
   };
   useEffect(() => {
     console.log(textOptions);
+    convert(textOptions);
   }, [textOptions]);
 
   useEffect(() => {
+    console.log(color);
+  }, [color]);
+
+  useEffect(() => {
     GetFonts();
-    convert(textOptions)
+    convert(textOptions);
   }, []);
+
 
   const convert = async (textOptions) => {
     try {
       const res = await axios({
-        url: "https://ascii-banner-backend-murex.vercel.app/convertText",
+        // url: "https://ascii-banner-backend-murex.vercel.app/convertText",
+        url: "http://localhost:8000/convertText",
         method: "post",
         data: { textOptions }
       });
@@ -85,67 +111,84 @@ function App() {
     }
   };
 
+  const handleCaptureClick = async () => {
+    if(DivRef){
+    const canvas = await html2canvas(DivRef.current);
+    const dataURL = canvas.toDataURL('image/png');
+    downloadjs(dataURL, 'download.png', 'image/png');
+    }
+  };
+
   return (
-    <div style={{ height: '500px', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-      <h1>ASCII BANNER GENERATOR</h1>
-      <div style={{ marginBottom: 20 }}>
-        fonts: <select defaultValue={textOptions.font} name="font" onChange={handleChange} >
-          <option value="" disabled>Select an option</option>
-          <option value={textOptions.font} >Ghost</option>
-          {fonts.map((option, index) => (
-            <option key={index} value={option}>
-              {option}
-            </option>
-          ))}
-        </select>
-        Text:<input defaultValue={textOptions.text} name='text' value={textOptions.text} onChange={handleChange} type='text' style={{ marginRight: 10, marginLeft: 10 }} />
-        <button onClick={() => convert(textOptions)}>Convert</button>
+    <div style={{ width: "100vw", height: "100vh", overflowY: 'auto' }}>
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+        <h1>ASCII BANNER GENERATOR</h1>
+        <div style={{ marginBottom: 20 }}>
+          fonts: <select defaultValue={textOptions.font} name="font" onChange={handleChange} >
+            <option value="" disabled>Select an option</option>
+            <option value={textOptions.font} >Ghost</option>
+            {fonts.map((option, index) => (
+              <option key={index} value={option}>
+                {option}
+              </option>
+            ))}
+          </select>
+          Text:<input defaultValue={textOptions.text} name='text' value={textOptions.text} onChange={handleChange} type='text' style={{ marginRight: 10, marginLeft: 10 }} />
+         
+        </div>
+        <div style={{ marginBottom: 15 }}>
+          Horizontal Layout:
+          <select defaultValue={textOptions.horizontallayout} name="horizontallayout" onChange={handleChange} style={{ marginRight: 10, marginLeft: 10 }}  >
+            {Layouts.map((option, index) => (
+              <option key={index} value={option}>
+                {option}
+              </option>
+            ))}
+          </select>
+          Vertical Layout:
+          <select defaultValue={textOptions.verticallayout} name="verticallayout" onChange={handleChange} >
+            {Layouts.map((option, index) => (
+              <option key={index} value={option}>
+                {option}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div style={{ marginBottom: 15 }}>
+          width:
+          <input type='number' defaultValue={textOptions.width} name="width" onChange={handleChange} style={{ marginRight: 20 }} >
+          </input>
+          WhiteSpaceBreak:
+          <select defaultValue={textOptions.whitespaceBreak} name="whitespaceBreak" onChange={handleSpaceChange} >
+            <option value="On">On</option>
+            <option value="Off">Off</option>
+          </select>
+        </div>
+        <div style={{ marginBottom: 20 }}>
+          Select Background Color : <button onClick={() => handlebgColorPick() }>Pick</button>
+        </div>
+        <div style={{ marginBottom: 20 }}>
+          Select Foreground Color : <button onClick={() => handlefgColorPick() }>Pick</button>
+        </div>
+        {/* {ChooseColor && <ColorPicker hideInput color={color} onChange={setColor} />} */}
+
+        <div style={{ display: 'flex' }}>
+          <div>
+            <div style={{ marginRight: 20,display:'flex' }}>
+              {ChoosebgColor && <h4 style={{marginRight:10}}>Picking Background Color : </h4>}
+              {ChoosefgColor && <h4 style={{marginRight:10}}> Picking Foreground Color : </h4>}
+              {ChoosebgColor && <ColorPicker hideInput={["rgb", "hsv"]} color={color} onChange={setColor} />}
+              {ChoosefgColor && <ColorPicker hideInput={["rgb", "hsv"]} color={fgcolor} onChange={setfgColor} />}
+            </div>
+          </div>
+          <div>
+            <div className="Mybanner" ref={DivRef} style={{ backgroundColor: color.hex, color: fgcolor.hex, padding: 15, maxHeight: "600px" }}>
+              <pre>{newText}</pre>
+            </div>
+          </div>
+        </div>
+        <button style={{marginTop:10}} onClick={handleCaptureClick}>Download</button>
       </div>
-      <div style={{ marginBottom: 15 }}>
-        Horizontal Layout:
-        <select defaultValue={textOptions.horizontallayout} name="horizontallayout" onChange={handleChange} style={{ marginRight: 10, marginLeft: 10 }}  >
-          {Layouts.map((option, index) => (
-            <option key={index} value={option}>
-              {option}
-            </option>
-          ))}
-
-
-        </select>
-
-       Vertical Layout:
-        <select defaultValue={textOptions.verticallayout} name="verticallayout" onChange={handleChange} >
-          {Layouts.map((option, index) => (
-            <option key={index} value={option}>
-              {option}
-            </option>
-          ))}
-
-
-        </select>
-      </div>
-      <div style={{ marginBottom: 15 }}>
-        width:
-        <input type='number' defaultValue={textOptions.width} name="width" onChange={handleChange} style={{ marginRight: 20 }} >
-
-
-        </input>
-
-        WhiteSpaceBreak:
-        <select defaultValue={textOptions.whitespaceBreak} name="whitespaceBreak" onChange={handleSpaceChange} >
-          <option value="On">On</option>
-          <option value="Off">Off</option>
-
-        </select>
-      </div>
-
-      <div style={{ backgroundColor: "black", color: 'white', padding: 15 }}>
-
-        <pre>{newText}</pre>
-
-
-      </div>
-
     </div>
   );
 }
